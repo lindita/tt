a7db07765b73620a5ec4f369b509162836eeaba9
lindita <>
2022-05-30 11:48:22 +0800

    c

--- internal/data/model/ttModel.go
+++ internal/data/model/ttModel.go
@@ -4,10 +4,10 @@ type TtModel struct {
 	BaseModel
 
 	UserId uint64 `gorm:"column:userId;type:bigint(20) unsigned;not null;default:0"` // 用户id
-	name   string `gorm:"column:alipayUserId;type:varchar(45);not null;default:''"`  //
+	name   string `gorm:"column:name;type:varchar(45);not null;default:''"`  //
 
 }
 
 func (m *TtModel) TableName() string {
-	return "stt_alipay_bind_log"
+	return "tt"
 }
